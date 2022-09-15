3f8cf45af306c74fbe71d15bed84947b2cf7aadd
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-09-16 01:08:39 +0300

    create task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,13 +4,33 @@ const galleryRef = document.querySelector('.gallery');
 const itemsMarkup = createItemsImageMarkup(galleryItems);
 
 galleryRef.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryRef.addEventListener('click', onGalleryContainerClick);
 
-function createItemsImageMarkup(items) {
-	return items
-		.map(({ preview, original, description }) => {
-			return `<a class="gallery__item" href="${original}">
+function createItemsImageMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
                         <img class="gallery__image" src="${preview}" alt="${description}" />
-                    </a>`;
-		})
-		.join('');
+			  </a>`;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  onOpenModalWindow();
+}
+
+function onOpenModalWindow() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+  return lightbox;
 }
