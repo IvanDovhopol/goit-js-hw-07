4c7022c986925d4b6792f35be83fb653b3748654
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-09-15 03:42:35 +0300

    create task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// Plan =>
-/*
- * 1) Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-
- * 2) Реализация делегирования на div.gallery и получение url большого изображения.
- * 
- * 3) Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
- * Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
- * 
- * 4) Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
- * 
- * 5) Замена значения атрибута src элемента <img> в модальном окне перед открытием.
- * Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
- */
 
 const galleryRef = document.querySelector('.gallery');
 const itemsMarkup = createItemsImageMarkup(galleryItems);
@@ -22,9 +6,9 @@ const itemsMarkup = createItemsImageMarkup(galleryItems);
 galleryRef.insertAdjacentHTML('beforeend', itemsMarkup);
 
 function createItemsImageMarkup(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
+	return items
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
             <img
                 class="gallery__image"
@@ -34,19 +18,37 @@ function createItemsImageMarkup(items) {
             />
         </a>
     </div> `;
-    })
-    .join('');
+		})
+		.join('');
 }
 
 galleryRef.addEventListener('click', onGalleryContainerClick);
 
+let instance = '';
+
 function onGalleryContainerClick(e) {
-  e.preventDefault();
-  const isClassGalleryImage = e.target.classList.contains('gallery__image');
+	e.preventDefault();
 
-  if (!isClassGalleryImage) {
-    return;
-  }
+	if (e.target.nodeName !== 'IMG') {
+		return;
+	}
 
-  console.log(e.target.attributes.alt);
+	onOpenModalWindow(e);
+	onCloseModalWindowKey(e);
+
+	galleryRef.addEventListener('keydown', onCloseModalWindowKey);
+}
+
+function onOpenModalWindow(e) {
+	instance = basicLightbox.create(`
+	    <img src="${e.target.dataset.source}" width="800" height="600">
+	`);
+
+	instance.show();
+}
+
+function onCloseModalWindowKey(e) {
+	if (e.code === 'Escape') {
+		instance.close();
+	}
 }
